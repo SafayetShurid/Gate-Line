4d803ccb1bc609566f52d0d6e52973163a36a65a
Shurid <safayet.shurid@netmediamobile.com>
2021-02-10 20:26:28 +0600

    prefabs

---
+++ Assets/Gate Line/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+
+    public CircleType type;
+
+    public enum CircleType
+    {
+        Orange,Green
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
